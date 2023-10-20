9b82f3784f167d87a71d6b5437ada716dcc06b5c
gg9192 <42902579+gg9192@users.noreply.github.com>
2023-10-19 23:42:27 -0500

    done with ast

--- regexAST.py
+++ regexAST.py
@@ -2,24 +2,52 @@ class Parens:
     def __init__(self):
         self.what = None
 
+    def __repr__(self):
+        return "(" + str(self.what) + ")"
+
 class OneOrMore:
     def __init__(self):
         self.what = None
 
+    def __repr__(self):
+        return str(self.what) + "+"
+
 class ZeroOrMore:
     def __init__(self):
         self.what = None
+
+    def __repr__(self):
+        return str(self.what) + "*"
 
 class follows:
     def __init__(self):
         self.first = None
         self.second = None
 
+    def __repr__(self):
+        return str(self.first) + str(self.second)
+
 class Or:
     def __init__(self):
         self.first = None
         self.second = None
 
+    def __repr__(self):
+        return str(self.first) + " + " + str(self.second)
+
 class RegexAST:
     def __init__(self):
         self.root = None
+
+    def __repr__(self):
+        return str(self.root)
+    
+tree = RegexAST()
+tree.root = ZeroOrMore()
+what = Parens()
+orr = Or()
+what.what = orr
+tree.root.what = what
+orr.first = "A"
+orr.second = "B"
+print(tree)
