d463d39c10889371d32ef1f0b8f1ef270464652f
gg9192 <42902579+gg9192@users.noreply.github.com>
2023-10-21 14:26:28 -0500

    changed readme

--- nfa.py
+++ nfa.py
@@ -1,18 +1,38 @@
+import graphviz 
+import os
 """This class represents"an NFA"""
-
 class NFA():
     def __init__(self):
+        # Nested hashmap, first 
         self.edges = {}
         self.startStates = set()
         self.acceptingStates = set()
         self.states = set()
     
-    "sets the starting states"
+    """sets the starting states"""
     def setStartingStates(self, states:list):
         for state in states:
             self.startStates.add(state)
     
-    def addEdge(self, state:int, char):
-        pass
+    """Adds the given edge"""
+    def addEdge(self, state:int, char:str):
+        if state in self.edges:
+            self.edges[state].add(char)
+        else:
+            s = set()
+            s.add(char)
+            self.edges[state] = s
 
-print(type('c'))
+    """sets the states"""
+    def setStates(self, states:list):
+        for state in states:
+            self.states.add(state)
+
+    def convertToDot():
+        pass
+    
+    
+
+
+
+        
